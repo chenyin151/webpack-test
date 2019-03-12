b8b463b434351fa523f71d45ce2c4f8161af376a
chenyin <cyin@163.com>
2019-03-12 17:42:37 +0800

    在加载css的时候如果我们每次都require('style-loader!css-loader!./style.css')，这样写未免多啰嗦，我们可有通过在命令行中加入：webpack hello.js hello.bundle.js --module-bind 'css=style-loader!css-loader'，这样就把css-loader和style-loader加入

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 require('./world'); //把world.js导入进来
-require('style-loader!css-loader!./style.css'); //导入CSS文件,我们要给它指定一个css-loader才能正确解析CSS
+require('./style.css'); //导入CSS文件,我们要给它指定一个css-loader才能正确解析CSS
 function hello(str){
     alert(str);
 }
